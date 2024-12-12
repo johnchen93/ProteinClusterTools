d108fc3c7ee4893f81574b799e6442caed16bd9f
John Chen <jo.chn.93@gmail.com>
2024-12-12 14:21:39 +1100

    new build, continue MMseq clustering, type consistency in representative functions

--- package/src/proteinclustertools/tools/vector_cluster_rep.py
+++ package/src/proteinclustertools/tools/vector_cluster_rep.py
@@ -9,9 +9,9 @@ def ClusterVectorRepresentative(clusters, level, vector_dict, target_clusters):
     target_clusters: list of clusters to analyze. Can be int or str. Ints are converted to str.
 
     """
-    clusters=clusters.astype(str)
+    clusters=clusters[['id',level]].astype(str)
     target_clusters=[str(x) for x in target_clusters]
-    
+
     cluster_ids=clusters.groupby(level)['id'].apply(list).to_dict()
 
     cluster_reps={}
